68b359a049cc2654966a1f114c25c03e83063390
Julia Yarba <sha1-2dd0cdca2ab13f746e6bcee07ed96db925a559da@cern.ch>
2008-11-21 20:39:42 +0000

    add prototype for Py6 hadronizer, towards GenInt redesign

---
+++ test/Py6HadGenFilter_cfg.py
@@ -0,0 +1,86 @@
+import FWCore.ParameterSet.Config as cms
+
+### from Configuration.GenProduction.PythiaUESettings_cfi import *
+
+process = cms.Process("TEST")
+process.load("FWCore.Framework.test.cmsExceptionsFatal_cff")
+
+
+process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
+    py6gf = cms.PSet(
+        initialSeed = cms.untracked.uint32(123456789),
+        engineName = cms.untracked.string('HepJamesRandom')
+    )
+)
+
+
+## process.load("Configuration.GenProduction.PythiaUESettings_cfi")
+
+# The following three lines reduce the clutter of repeated printouts
+# of the same exception message.
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.destinations = ['cerr']
+process.MessageLogger.statistics = []
+process.MessageLogger.fwkJobReports = []
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(5))
+
+process.source = cms.Source("EmptySource")
+
+process.py6gf = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(True),
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    comEnergy = cms.double(10000.0),
+    PythiaParameters = cms.PSet(
+
+###       pythiaUESettingsBlock,
+        pythiaUESettings = cms.vstring('MSTJ(11)=3     ! Choice of the fragmentation function', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+#           'MSTP(51)=10042     ! CTEQ6L1 structure function chosen', 
+#            'MSTP(52)=2     ! work with LHAPDF', 
+            'MSTP(81)=1     ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model', 
+            'MSTU(21)=1     ! Check on possible errors during program execution', 
+            'PARP(82)=1.8387   ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1960. ! sqrts for which PARP82 is set', 
+            'PARP(83)=0.5   ! Multiple interactions: matter distrbn parameter', 
+            'PARP(84)=0.4   ! Multiple interactions: matter distribution parameter', 
+            'PARP(90)=0.16  ! Multiple interactions: rescaling power', 
+            'PARP(67)=2.5    ! amount of initial-state radiation', 
+            'PARP(85)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(86)=1.0  ! gluon prod. mechanism in MI', 
+            'PARP(62)=1.25   ! ', 
+            'PARP(64)=0.2    ! ', 
+            'MSTP(91)=1     !', 
+            'PARP(91)=2.1   ! kt distribution', 
+            'PARP(93)=15.0  ! '),
+
+        processParameters = cms.vstring('MSEL=0         ! User defined processes', 
+            'MSUB(11)=1     ! Min bias process', 
+            'MSUB(12)=1     ! Min bias process', 
+            'MSUB(13)=1     ! Min bias process', 
+            'MSUB(28)=1     ! Min bias process', 
+            'MSUB(53)=1     ! Min bias process', 
+            'MSUB(68)=1     ! Min bias process', 
+            'MSUB(92)=1     ! Min bias process, single diffractive', 
+            'MSUB(93)=1     ! Min bias process, single diffractive', 
+            'MSUB(94)=1     ! Min bias process, double diffractive', 
+            'MSUB(95)=1     ! Min bias process'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+process.GEN = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('TestMB.root')
+)
+
+process.p = cms.Path(process.py6gf)
+process.outpath = cms.EndPath(process.GEN)
+
+process.schedule = cms.Schedule(process.p, process.outpath)
